522066eb9eababe6e61bd992e33081c4e8594aa8
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2026-06-06 09:53:08 +0900

    Add; 一回だけdragできるようにする

--- Assets/Scripts/Road/DraggableRoad.cs
+++ Assets/Scripts/Road/DraggableRoad.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class DraggableRoad : MonoBehaviour
 {
+    private Camera mainCamera;
     private Vector3 screenPoint;    // オブジェクトのスクリーン座標を保存する変数
     private Vector3 offset;         // オブジェクトの位置とマウスの位置の差分を保存する変数
-    private Camera mainCamera;
+    private bool wasDragged = false;   // ドラッグされたかどうかを保存する変数
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,6 +23,7 @@ public class DraggableRoad : MonoBehaviour
     /// </summary>
     private void OnMouseDown()
     {
+        if (wasDragged) return;
         screenPoint = mainCamera.WorldToScreenPoint(transform.position);  // オブジェクトのワールド座標をスクリーン座標に変換して保存
         offset = transform.position - mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));  // オブジェクトの位置とマウスの位置の差分を計算して保存 
     }
@@ -31,8 +33,17 @@ public class DraggableRoad : MonoBehaviour
     /// </summary>
     private void OnMouseDrag()
     {
+        if (wasDragged) return;
         Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);  // マウスの現在の位置をスクリーン座標で取得
         Vector3 currentPosition = mainCamera.ScreenToWorldPoint(currentScreenPoint) + offset;   // スクリーン座標をワールド座標に変換し、オフセットを加算して新しい位置を計算
         transform.position = currentPosition;
+    }
+
+    /// <summary>
+    /// オブジェクトがドラッグされた後の処理
+    /// </summary>
+    private void OnMouseUp()
+    {
+        wasDragged = true;   // ドラッグされたことを記録
     }
 }
